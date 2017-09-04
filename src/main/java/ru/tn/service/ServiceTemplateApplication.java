97010d957fded9e429ee00e09a8e98541c4e3c6f
RNemykin <nemykinroman@gmail.com>
2017-09-04 23:21:19 +0300

    init

---
+++ src/main/java/ru/tn/service/ServiceTemplateApplication.java
@@ -0,0 +1,12 @@
+package ru.tn.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTemplateApplication.class, args);
+	}
+}
